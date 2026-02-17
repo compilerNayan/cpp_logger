5e5cbb825e1eb72053c44fcfc3b6947a6c6f375c
Nayan Kurude <compilerNayan@gmail.com>
2026-02-17 15:04:54 +0530

    kjdajks

--- src/ArduinoLogSink.h
+++ src/ArduinoLogSink.h
@@ -4,17 +4,22 @@
 
 #include <StandardDefines.h>
 #include "ILogSink.h"
+#include <IArduinoRemoteStorage.h>
 #include <Arduino.h>
 
 /* @Component */
 class ArduinoLogSink final : public ILogSink {
+    /* @Autowired */
+    Private IArduinoRemoteStoragePtr remoteStorage;
+
     Public
         ArduinoLogSink() = default;
-        
+
         Public Virtual ~ArduinoLogSink() override = default;
-        
+
         Public Virtual Void WriteLog(CStdString& message) override {
             Serial.println(message.c_str());
+            remoteStorage->StoreLog(message);
         }
 };
 
